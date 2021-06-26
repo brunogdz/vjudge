a37a038fc0e7e6f944162bd6dc92bec7483e84d1
Bruno Gomes <brunog.vix@gmail.com>
2021-06-26 12:31:51 -0300

    2413 - Busca na Internet solution

---
+++ 2413.cpp
@@ -0,0 +1,15 @@
+/*
+solution 2413 - Busca na Internet URI ONLINE JUDGE
+*/
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    cout << (t*2)*2 << endl;
+    return 0;
+}
