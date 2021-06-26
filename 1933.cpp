dd01190338182a31a2074ca4f77bf82dc7087179
Bruno Gomes <brunog.vix@gmail.com>
2021-06-26 12:39:56 -0300

    solution 1933 - Tri-du URI ONLINE JUDGE

---
+++ 1933.cpp
@@ -0,0 +1,21 @@
+/*
+solution 1933 - Tri-du URI ONLINE JUDGE
+*/
+
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int A,B;
+    cin >> A >> B;
+    if(A > B){
+        cout << A << endl;
+    }else{
+        cout << B << endl;
+    }
+    
+    return 0;
+}
